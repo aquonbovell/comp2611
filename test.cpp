57539301dd38f49c644b9bf6db1bd64b24939bc7
Aquon Bovell <aquon.bovell@outlook.com>
2023-02-18 10:51:09 -0400

    first commit

--- test.cpp
+++ test.cpp
@@ -1,28 +1,10 @@
 // #include "Queue.h"
-#include "Stack.h"
+// #include "Stack.h"
 #include <iostream>
-using std::cout, std::boolalpha;
+#include <algorithm>
 
 int main(){
-  // Queue q = Queue();
-  // q.enqueue(3,"hell0",38.3);
-  // q.enqueue(3,"lokk",38.3);
-  // // std::cout<<std::boolalpha<<q.isEmpty();
-  // q.enqueue(3,"ojjj",38.3);
-  // q.dequeue();
-  // q.dequeue();
-  // std::cout<<q.getAllData();
-  // std::cout<<q.dequeue();
-  // std::cout<<q.size();
-  // std::cout<<q.dequeue();
-  
-  
-  Stack st = Stack();
-  std::cout<<st.pop();
-  st.push("Jake");
-  st.push("Justion");
-  // std::cout<<st.pop();
-  st.push("Justin");
-  std::cout<<st.getAllData();
-  std::cout<<st.size();
+  std::string str = "\nDecember";
+  str.erase(std::remove(str.begin(), str.end(), '\n'), str.end());
+  std::cout<<str;
 }
