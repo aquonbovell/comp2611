11b912a9a17d4df1eca439996a4733f0308af766
Aquon Bovell <aquon.bovell@outlook.com>
2023-03-01 20:35:32 -0400

    added comments

--- project-1/test.cpp
+++ project-1/test.cpp
@@ -1,15 +1,12 @@
-// #include "PQueue.h"
-// #include "Deque.h"
-// #include "Queue.h"
-// #include "Stack.h"
-// #include <iostream>
+#include "PQueue.h"
+#include "Deque.h"
+#include "Queue.h"
+#include "Stack.h"
+#include <iostream>
 
-// int main(){
-//   Stack *node = new Stack();
-//   std::cout<<node->createStack("./catalog.txt")<<std::endl;
-//   std::cout<<node->createStack("./catalog.txt")<<std::endl;
-//   // std::cout<<dq->createDeque("../catlog.txt");
-//   // node->enqueueHead("Jan", 1332, "Bubu", "Song", "Jam Rock", 14);
-//   std::cout<<node->displayAll();
-//   return 0;
-// }
+int main(){
+  const string op = "jdjosdoss";
+  string k = op;
+  
+  return 0;
+}
